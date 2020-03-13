091aed9c1a18a75986db8545190a99f441e1868d
Peter Rose <peterroseq1@gmail.com>
2020-03-13 16:04:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = []
   array << string.split 
   array2 = []
-  array.each do |word| array2 << "#{word}"
+  array.each do |word| array2 << "#{word}".reverse
   end
   array2
 end
